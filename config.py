bb592c8e5acbc9c832a5d0277862d098dc1ee6e5
TiredBob <supercrackmunkey@gmail.com>
2025-08-10 11:03:03 -0400

    added notes to the config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 TOKEN = "YOUR_BOT_TOKEN_HERE"
-COMMAND_NAME = "fortune"
-COMMAND_ALIAS = "ft"
+COMMAND_NAME = "fortune" # This is the furtune tellers name as well as the command. Update this.
+COMMAND_ALIAS = "ft" # An alias to the command. Useful if you name the fortune teller something ridicular or hard to type.
 GEMINI_API_KEY = "YOUR_GEMINI_API_KEY_HERE"
